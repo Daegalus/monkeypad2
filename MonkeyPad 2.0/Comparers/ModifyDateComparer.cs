f2e09a1f131c1c346e9c315443c919dfe7e60fca
Yulian Kuncheff <yulian@kuncheff.com>
2011-09-03 21:17:33 -0700

    Add ModifyDateComparer Use BinarySearch to insert the not, so that it gets added sorted. Fix up code and processing.

---
+++ MonkeyPad 2.0/Comparers/ModifyDateComparer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using MonkeyPad2.Notes;
+
+namespace MonkeyPad2.Comparers
+{
+    public class ModifyDateComparer : IComparer<Note>
+    {
+        #region IComparer<Note> Members
+
+        public int Compare(Note x, Note y)
+        {
+            return decimal.Compare(y.ModifyDate, x.ModifyDate); // sort by modifydate
+        }
+
+        #endregion
+    }
+}
